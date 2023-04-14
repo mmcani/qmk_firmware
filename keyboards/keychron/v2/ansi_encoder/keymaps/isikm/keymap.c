088ad9784af2e6efd74f744a437165779bae7d88
Can <mmtcan@gmail.com>
2023-04-14 15:10:02 +0200

    space cadet added, updated layers further

--- keyboards/keychron/v2/ansi_encoder/keymaps/isikm/keymap.c
+++ keyboards/keychron/v2/ansi_encoder/keymaps/isikm/keymap.c
@@ -89,49 +89,52 @@ tap_dance_action_t tap_dance_actions[] = {
 enum layers{
     GAME_BASE = 0,
     WIN_BASE,
-    FN1,
-    FN2,
+    RGB_MEDIA,
+    FUNCTIONS,
 };
 
 #define LALT_LB LALT_T(KC_LBRC)
 #define RALT_RB RALT_T(KC_RBRC)
 #define WN_LOCK LWIN(KC_L)
 #define KC_TERM LCA(KC_T)
+#define K_MEDIA TT(RGB_MEDIA)
+#define K_FUNC TT(FUNCTIONS)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [GAME_BASE] = LAYOUT_ansi_67(
         KC_ESC,  KM_1,     KM_2,     KM_3,    KM_4,    KM_5,    KM_6,    KM_7,    KM_8,    KM_9,    KM_10,    KM_11,    KM_12,    KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         KC_CAPS, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,           KC_HOME,
         SC_LSPO,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            SC_RSPC, KC_UP,
-        KC_LCTL, KC_LWIN,  LALT_LB,                             KC_SPC,                             RALT_RB,  TT(FN2),  TT(FN1),  KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LWIN,  LALT_LB,                             KC_SPC,                             RALT_RB,  K_FUNC,   K_MEDIA,  KC_LEFT, KC_DOWN, KC_RGHT),
     [WIN_BASE] = LAYOUT_ansi_67(
-        KC_ESC,  KM_1,     KM_2,     KM_3,    KM_4,    KM_5,    KM_6,    KM_7,    KM_8,    KM_9,    KM_10,    KM_11,    KM_12,    KC_BSPC,          KC_MUTE,
+        KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         KC_CAPS, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,           KC_HOME,
         SC_LSPO,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            SC_RSPC, KC_UP,
-        KC_LCTL, KC_LWIN,  LALT_LB,                             KC_SPC,                             RALT_RB,  TT(FN2),  TT(FN1),  KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LWIN,  LALT_LB,                             KC_SPC,                             RALT_RB,  K_FUNC,   K_MEDIA,  KC_LEFT, KC_DOWN, KC_RGHT),
 
-    [FN1] = LAYOUT_ansi_67(
+    [RGB_MEDIA] = LAYOUT_ansi_67(
         KC_GRV,  KC_BRID,  KC_BRIU,  _______, _______, _______, _______, _______, _______, _______, KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,          KC_MUTE,
         RGB_TOG, _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,          _______,
         RGB_TOG, RGB_MOD,  RGB_HUI,  RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______,            _______,          _______,
         _______,           RGB_RMOD, RGB_HUD, RGB_SAD, RGB_SPD, _______, NK_TOGG, _______, _______, _______,  _______,            _______, _______,
-        _______, _______,  _______,                             _______,                            _______,  KC_NO,    KC_NO,  _______, _______, _______),
+        _______, _______,  _______,                             _______,                            _______,  KC_NO,    K_MEDIA,  _______, _______, _______),
 
-    [FN2] = LAYOUT_ansi_67(
-        KC_TILD, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          KC_MUTE,
-        _______, _______,  _______,  _______, _______, KC_TERM, _______, _______, _______, _______, _______,  _______,  _______,  _______,          _______,
-        _______, _______,  _______,  _______, _______, _______, _______, _______, _______, WN_LOCK, _______,  _______,            _______,          _______,
-        _______,           _______,  _______, KC_CALC, _______, _______, _______, KC_MYCM, _______, _______,  _______,            _______, _______,
-        _______, _______,  _______,                             _______,                            _______,  KC_NO,    KC_NO,  _______, _______, _______)
+    [FUNCTIONS] = LAYOUT_ansi_67(
+        KC_TILD, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          C(KC_0),
+        _______, _______,  _______,  _______, _______, KC_TERM, _______, _______, _______, _______, _______,  _______,  _______,  _______,          KC_PAGE_UP,
+        _______, _______,  _______,  _______, _______, _______, _______, _______, _______, WN_LOCK, _______,  _______,            _______,          KC_PAGE_DOWN,
+        _______,           _______,  _______, KC_CALC, _______, _______, _______, KC_MYCM, _______, _______,  _______,            _______, KC_HOME,
+        _______, _______,  _______,                             _______,                            _______,  K_FUNC,   KC_NO,    _______, KC_END,  _______)
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [GAME_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [FN1]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [FN2]   = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [RGB_MEDIA] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [FUNCTIONS] = { ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL))},
 };
 #endif // ENCODER_MAP_ENABLE
 /*====================================KEY MAPS====================================*/
@@ -139,8 +142,8 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 
 
 /*====================================RGB MODS====================================*/
-#define FN1_LAYER_COLOR RGB_ORANGE
-#define FN2_LAYER_COLOR RGB_SPRINGGREEN
+#define RGB_LAYER_COLOR RGB_ORANGE
+#define FUNC_LAYER_COLOR RGB_SPRINGGREEN
 
 // helper function that sets no trans keys of the layer to the provided color, trans keys are set turned off
 void rgb_matrix_set_color_by_keycode(uint8_t led_min, uint8_t led_max, uint8_t layer, uint8_t red, uint8_t green, uint8_t blue) {
@@ -159,13 +162,14 @@ void rgb_matrix_set_color_by_keycode(uint8_t led_min, uint8_t led_max, uint8_t l
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     uint8_t current_layer = get_highest_layer(layer_state|default_layer_state);
     switch (current_layer) {
-        case FN1:
-            rgb_matrix_set_color_by_keycode(led_min, led_max, current_layer, FN1_LAYER_COLOR);
+        case RGB_MEDIA:
+            rgb_matrix_set_color_by_keycode(led_min, led_max, current_layer, RGB_LAYER_COLOR);
             break;
-        case FN2:
-            rgb_matrix_set_color_by_keycode(led_min, led_max, current_layer, FN2_LAYER_COLOR);
+        case FUNCTIONS:
+            rgb_matrix_set_color_by_keycode(led_min, led_max, current_layer, FUNC_LAYER_COLOR);
             break;
         case WIN_BASE:
+        case GAME_BASE:
         default:
             if (!rgb_matrix_get_flags()) {  // if no led layers are set, set it to black, else the last led layers setting will take over
                 rgb_matrix_set_color_by_keycode(led_min, led_max, current_layer, RGB_OFF);
