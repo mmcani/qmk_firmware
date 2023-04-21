892d6750fc6fbd3b2fb7704ed68054a8abcaf24f
Can <mmtcan@gmail.com>
2023-04-21 14:04:17 +0200

    final keymap for k3 pro

--- keyboards/keychron/k7_pro/ansi/rgb/keymaps/isikm/keymap.c
+++ keyboards/keychron/k7_pro/ansi/rgb/keymaps/isikm/keymap.c
@@ -13,7 +13,6 @@ enum layers{
 
 #define LALT_LB LALT_T(KC_LBRC)
 #define RALT_RB RALT_T(KC_RBRC)
-#define WN_LOCK LWIN(KC_L)
 #define KC_TERM LCA(KC_T)
 #define K_MEDIA TT(RGB_MEDIA)
 #define K_FUNC TT(FUNCTIONS)
@@ -21,32 +20,32 @@ enum layers{
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [GAME_BASE] = LAYOUT_ansi_68(
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,           KC_DEL,
+     QK_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,           KC_DEL,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,           KC_HOME,
      KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,            KC_PGUP,
      SC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                      SC_RSPC,  KC_UP,   KC_PGDN,
      KC_LCTL,  KC_LGUI,  LALT_LB,                                KC_SPC,                                 RALT_RB,  K_FUNC,   K_MEDIA, KC_LEFT,  KC_DOWN, KC_RGHT),
 
 [WIN_BASE] = LAYOUT_ansi_68(
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,           KC_DEL,
+     QK_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,           KC_DEL,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,           KC_HOME,
      KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,            KC_PGUP,
      SC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                      SC_RSPC,  KC_UP,   KC_PGDN,
      KC_LCTL,  KC_LGUI,  LALT_LB,                                KC_SPC,                                 RALT_RB,  K_FUNC,   K_MEDIA,  KC_LEFT,  KC_DOWN, KC_RGHT),
 
 [RGB_MEDIA] = LAYOUT_ansi_68(
-     _______,  KC_BRID,  KC_BRIU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_MUTE,
-     _______,  BT_HST1,  BT_HST2,  BT_HST3,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_VOLU,
-     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_VOLD,
-     _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,
-     _______,  _______,  _______,                                _______,                                _______,  KC_NO,    K_MEDIA,  KC_MPRV,  KC_MPLY,  KC_MNXT),
+     BAT_LVL,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_BRID,  KC_BRIU,            EE_CLR,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            QK_RBT,
+     _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            QK_MAKE,
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,
+     _______,  _______,  _______,                                _______,                                _______,  KC_NO,    K_MEDIA,  _______,  _______,  _______),
 
 [FUNCTIONS] = LAYOUT_ansi_68(
-     _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            _______,
-     _______,  _______,  _______,  _______,  _______,  KC_TERM,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  WN_LOCK,  _______,  _______,            _______,            _______,
-     _______,  _______,  _______,  KC_CALC,  _______,  BAT_LVL,  NK_TOGG,  KC_MYCM,  _______,  _______,  _______,                      _______,  _______,  _______,
-     _______,  _______,  _______,                                _______,                                _______,  K_FUNC,   KC_NO,    _______,  _______,  _______),
+     KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            KC_MUTE,
+     _______,  BT_HST1,  BT_HST2,  BT_HST3,  _______,  KC_TERM,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_VOLU,
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_VOLD,
+     _______,  _______,  _______,  KC_CALC,  _______,  _______,  NK_TOGG,  KC_MYCM,  _______,  _______,  _______,                      _______,  _______,  _______,
+     _______,  _______,  _______,                                _______,                                _______,  K_FUNC,   KC_NO,    KC_MPRV,  KC_MPLY,  KC_MNXT),
 
 [EXTRA] = LAYOUT_ansi_68(
      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,           _______,
